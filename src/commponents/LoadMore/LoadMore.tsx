c36c3315d874208cea1413f4ceedc7d7f91e5073
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-24 22:03:27 +0200

    Add files via upload

--- src/commponents/LoadMore/LoadMore.tsx
+++ src/commponents/LoadMore/LoadMore.tsx
@@ -1,1 +1,12 @@
+import { FC } from "react";
+import Button from "../Button/Button";
 
+interface LoadMoreProps {
+  onLoadMoreClick: () => void;
+}
+
+const LoadMore: FC<LoadMoreProps> = ({ onLoadMoreClick }) => {
+  return <Button onClick={onLoadMoreClick}>LoadMore</Button>;
+};
+
+export default LoadMore;
