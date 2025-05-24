a36cb4fb8eb5396abc9cc0469bd199fa1726414e
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-24 22:10:09 +0200

    Add files via upload

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,12 @@
-
+export interface ImageInfo {
+  id: string;
+  urls: {
+    small: string;
+    full: string;
+  };
+  alt_description: string;
+}
+export interface FetchImagesResponse {
+  results: ImageInfo[];
+  total_pages: number;
+}
