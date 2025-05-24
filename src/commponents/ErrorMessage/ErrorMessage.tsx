251d8acf5c525c7047335842ab12dc6918649e39
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-24 21:58:23 +0200

    Add files via upload

--- src/commponents/ErrorMessage/ErrorMessage.tsx
+++ src/commponents/ErrorMessage/ErrorMessage.tsx
@@ -1,1 +1,11 @@
+import { FC } from "react";
+import css from "./ErrorMessage.module.css";
 
+interface ErrorMessageProps {
+  message: string;
+}
+const ErrorMessage: FC<ErrorMessageProps> = ({ message }) => {
+  return <div className={css.errorMessage}>{message}</div>;
+};
+
+export default ErrorMessage;
