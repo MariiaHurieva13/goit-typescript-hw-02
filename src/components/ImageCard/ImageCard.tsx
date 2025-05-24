abcb7b8def6a32b5a7b4b05dcf5216587d1f7711
MariiaHurieva13 <Marygureva@gmail.com>
2025-05-24 22:45:24 +0200

    Add files via upload

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,1 +1,29 @@
+import { FC } from "react";
+import css from "./ImageCard.module.css";
+import { ImageInfo } from "../../types/types";
 
+interface ImageCardProps {
+  galleryItem: ImageInfo;
+  onClick: (id: string) => void;
+}
+
+const ImageCard: FC<ImageCardProps> = ({ galleryItem, onClick }) => {
+  const {
+    urls: { small },
+    alt_description,
+    id,
+  } = galleryItem;
+
+  return (
+    <div className={css.thumb}>
+      <img
+        src={small}
+        alt={alt_description}
+        className={css.cardImg}
+        onClick={() => onClick(id)}
+      />
+    </div>
+  );
+};
+
+export default ImageCard;
